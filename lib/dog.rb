4158b5bf3280c992b24ed89a35e39b774b600b44
John Tarleton Moore <tarletonmoore@gmail.com>
2022-08-20 11:33:22 -0400

    commit tree 78c0354e55442221a6db6ecaf0a4fda28b08fed0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
 
-end
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  puts lassie.name
+
+  
+
+  
+  odie = Dog.new
+  odie.name = "Odie"
+  
+  puts odie.name
+  
